8522416d981c49e7fad7e0c9ecf1e77c7833c167
agapxx <okontaagape@gmail.com>
2022-10-11 18:03:21 -0700

    done

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 101-square.py
 """Define a class Square."""
 
 
@@ -62,4 +63,8 @@ class Square:
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
